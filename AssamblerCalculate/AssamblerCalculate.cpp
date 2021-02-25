9631bd6df536cbc6db1f5ee62826ff6ea4a7fb89
SlavaPerryAyeKruchkovenko <71411231+SlavaPerryAyeKruchkovenko@users.noreply.github.com>
2021-02-25 11:05:40 +0500

    Update AssamblerCalculate.cpp

--- AssamblerCalculate/AssamblerCalculate.cpp
+++ AssamblerCalculate/AssamblerCalculate.cpp
@@ -1,4 +1,4 @@
-﻿// AssamblerCalculate.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// AssamblerCalculate.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
@@ -17,16 +17,26 @@ public:
 private:
     static char sign;
 public:
-    static int Add(int num1, int num2,bool isAdd)
+    static int Add(int num1, int num2)
     {
         int result;
-		if (!isAdd)
-			num2 *= -1;
         __asm
         {
             mov eax, num1;
             mov ebx, num2;
-            add eax, ebx;
+            adc eax, ebx;
+            mov result, eax;
+        }
+        return result;
+    }
+    static int Sub(int num1, int num2)
+    {
+        int result;
+        __asm
+        {
+            mov eax, num1;
+            mov ebx, num2;
+            sub eax, ebx;
             mov result, eax;
         }
         return result;
@@ -38,7 +48,7 @@ public:
         {
             mov eax, num1;
             mov ebx, num2;
-            mul ebx;
+            imul ebx;
             mov result, eax;
         }
         return result;
@@ -50,6 +60,7 @@ public:
         {
             mov eax, num1;
             mov ebx, num2;
+            mov edx, 0;
             div ebx;
             mov result, eax;
         }
@@ -60,7 +71,7 @@ static bool CheckConvert(string input)
 {
     try
     {
-        stoi(input);
+        int num = stoi(input);
         return true;
     }
     catch (const exception& error)
@@ -82,32 +93,23 @@ static int ReadLine(string name)
 }
 static int CalculateX(int nums[])
 {
-	int x = Calculate::Add(nums[0],nums[1],true);
-	x = Calculate::Mul(x, nums[2]);
-	x = Calculate::Add(x, nums[3], false);
-	x = Calculate::Div(x, nums[4]);
-	return x
+    int x = Calculate::Add(nums[0], nums[1]);
+    x = Calculate::Mul(x, nums[2]);
+    x = Calculate::Sub(x, nums[3]);
+    x = Calculate::Div(x, nums[4]);
+    return x;
 }
 int main()
 {
     int nums[5];
     for (char i = 'a'; i <= 'e'; i++)
     {  
-        nums[i - 'a'] = ReadLine(to_string(i));
+        string name="";
+        name = i;
+        nums[i - 'a'] = ReadLine(name);
     }
-    cout<<Calculate::Div(nums[0], nums[1]);
 	cout << ("Answer") << endl << CalculateX(nums);
+    delete nums;
     _getch();
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
